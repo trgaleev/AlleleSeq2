48c0674042658a37274a0b5de151b4e46614c67b
trgaleev <trgaleev@gmail.com>
2017-02-09 17:30:25 -0500

    reorganized; added STAR and combined diploid idx mode

---
+++ get_hetSNV_bed.py
@@ -0,0 +1,19 @@
+import sys
+
+with sys.stdin as in_vcf:
+    for line in in_vcf:
+        if line.startswith('#CHROM'): sample_col_idx = line.split().index(sys.argv[1])
+        if not line.startswith('#'):
+            CHROM,POS,ID,REF,ALT,QUAL,FILTER,INFO,FORMAT=line.split('\t')[:9]
+            if FILTER=='PASS':
+                GTl=line.split('\t')[sample_col_idx].split(':')[0].replace('/','|').split('|')
+                al=[REF]+ALT.split(',')
+                if al[int(GTl[0])]!=al[int(GTl[1])] and len(al[int(GTl[0])])==len(al[int(GTl[1])])==len(REF)==1:
+                    sys.stdout.write('\t'.join([    
+                                                   CHROM,
+                                                   str(int(POS)-1),
+                                                   POS,
+                                                   '_'.join([CHROM,POS,REF, al[int(GTl[0])], al[int(GTl[1])]])        
+                                               ])+'\n')
+            
+            
