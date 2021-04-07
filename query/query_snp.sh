f8eb211792167bf965f5a80ffb6ec29f210e71a1
leetx1010 <38360454+leetx1010@users.noreply.github.com>
2021-04-07 11:43:28 -0400

    Update query_snp.sh
    Output all SNPs from the query

--- query/query_snp.sh
+++ query/query_snp.sh
@@ -1,5 +1,5 @@
 if [[ $# -eq 0 ]] ; then
-    printf "Querying the ENTEx AS catalog.\nPlease enter the path to the input file, followed by at most three of the following types of keywords:\n      Assay (e.g. H3K27ac)\n      Individual (e.g. ENC-001)\n      Tissue (e.g. spleen)\nThe output will count the number of AS SNPs in the catalog from the query.\n"
+    printf "Querying the ENTEx AS catalog.\nPlease enter the path to the input file, followed by at most three of the following types of keywords:\n      Assay (e.g. H3K27ac)\n      Individual (e.g. ENC-001)\n      Tissue (e.g. spleen)\nThe output is all AS SNPs in the catalog from the query.\n"
     exit 0
 fi
 
@@ -10,4 +10,4 @@ q3="$4"
 
 
 echo "Querying $q1|$q2|$q3 on file $f"
-grep "$q1" $f | grep "$q2" | grep "$q3" | awk '{print $1,$2,$4,$NF}' | sort | uniq | awk '{print $NF}' | sort | uniq  -c | awk '{if($NF==1) print "AS "$1}'
+grep "$q1" $f | grep "$q2" | grep "$q3" | awk '{print $1,$2,$4,$NF}' | sort | uniq |  awk '{if($NF==1) print $0}'
